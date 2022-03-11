27b32d1112d66046e5fc0cc1d6604dd09dc15b21
Varun Kumar <nurav0402@gmail.com>
2022-03-11 09:35:58 +0530

    initial commit

---
+++ oswaldtruz/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from oswaldtruz.views import About, Contact, Home
+
+urlpatterns = [
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('securelogin/', admin.site.urls),
+    path('', Home, name='home'),
+    path('about/', About, name='about'),
+    path('contact', Contact, name='contact'),
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+
+    # order URls
+    path('orders/', include('orders.urls')),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
